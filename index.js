076ce917a2bd1353afa06e2f1c57f8003ca00a61
Sadman95 <mail.sakib.1995@gmail.com>
2021-11-10 13:37:01 +0600

    jwt token

--- index.js
+++ index.js
@@ -3,6 +3,33 @@ const app = express();
 const cors = require('cors');
 require('dotenv').config();
 const { MongoClient } = require('mongodb');
+const admin = require("firebase-admin");
+
+
+var serviceAccount = require("./doctors-portal-a687d-firebase-adminsdk-dp18h-4886f769c0.json");
+
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount)
+});
+
+
+async function verify (req, res, next){
+
+    if(req.headers?.authorization?.startsWith('Bearer ')){
+        const token = req.headers.authorization.split(' ')[1];
+
+        try{
+            const decodedUser = await admin.auth().verifyIdToken(token);
+            req.decodedEmail = decodedUser.email;
+        }
+        catch{
+
+        }
+    }
+
+    next();
+}
+
 
 
 
@@ -22,6 +49,7 @@ const database = client.db("doctorsPortal_db");
 const doctorsCollection = database.collection("doctors");
 const appointmentsCollection = database.collection("appointments");
 const usersAppointmentsCollection = database.collection('usersAppointments');
+const usersCollection = database.collection('users');
 
 
 
@@ -56,17 +84,73 @@ async function run(){
             res.send(result);
         })
 
+        //post new user:
+        app.post('/users', async(req, res)=> {
+            const user = req.body;
+            const result = await usersCollection.insertOne(user);
+            res.json(result);
+        })
+        //get users:
+        app.get('/users', async(req, res)=> {
+            const cursor = usersCollection.find({});
+            const query = await cursor.toArray();
+            res.send(query);
+        })
+        //put user:
+        app.put('/users', async(req, res) =>{
+            const user = req.body;
+            const filter = {email: user.email};
+            const options = {upsert: true};
+            const updateDoc = {$set: user};
+            const result = await usersCollection.updateOne(filter, updateDoc, options);
+            res.json(result);
+        })
+        // update a user as admin:
+        app.put('/users/admin', verify, async(req, res)=>{
+            const user = req.body;
+            const requester = req.decodedEmail;
+            if(requester){
+                const requesterAccount = await usersCollection.find({email: requester});
+                if(requesterAccount.role === 'admin'){
+                console.log(user);
+                const filter = {email: user.email};
+                const updateDoc = {$set: {role: 'admin'}};
+                const result = await usersCollection.updateOne(filter, updateDoc);
+                res.json(result);
+                }
+            }
+            else{
+                res.status(401).json({authorization: 'failed'})
+            }
+            
+        })
+
+        //admin check:
+        app.get('/users/admin/:email', async(req, res)=>{
+            const email = req.params.email;
+            const query = {email: email};
+            const user = await usersCollection.findOne(query);
+            let isAdmin = false;
+            if(user?.role === 'admin'){
+                isAdmin = true;
+            }
+            res.send({admin: isAdmin});
+        })
+
         //post users appointments:
         app.post('/allAppointments', async(req, res) =>{
             const userAppointment = req.body;
             const result = await usersAppointmentsCollection.insertOne(userAppointment);
             res.json(result);
         })
-        //get users appointments:
-        app.get('/allAppointments', async(req, res) =>{
-            const cursor = usersAppointmentsCollection.find({});
-            const query = await cursor.toArray();
-            res.send(query);
+        //get users appointments by email:
+        app.get('/allAppointments', verify, async(req, res) =>{
+            const email = req.query.email;
+            const date = new Date(req.query.date).toLocaleDateString();
+            const query = {email: email, date: date};
+            const cursor = usersAppointmentsCollection.find(query);
+            const result = await cursor.toArray();
+            res.send(result);
         })
 
     }
